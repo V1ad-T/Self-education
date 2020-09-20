edf5e3e3fc76581fdd6ed034eae51eb511bcc6b5
V1ad-T <vlad.goal.ua@gmail.com>
2020-09-20 23:25:28 +0300

    fixt display

---
+++ js/chek.js
@@ -0,0 +1,37 @@
+/* написать код который будет проверять число "X" на делители без остатка
+1. +запрос числа у пользователя
+2. проверка на числа на 0
+3. +проверка числа на string( реализовано преобразование)
+4. провкрка числа на undefind
+5. +вычисление делителей
+6. +вывод результата*/
+
+
+console.log("Start");
+let x = putInNamber();
+let  arreyD = [];
+
+
+if ( x ) {
+  console.log("GO");
+  calculet(x);
+} else {
+    console.log('vasya');
+    putInNamber();
+};
+
+function putInNamber() {
+  let n = +prompt('Введите число для поиска', 12);
+  return n;
+};
+
+function calculet(x) {
+  for ( let i = 1; i <= x; i++) {
+    if ( x % i === 0 ) {
+      arreyD.push(i);
+    }
+  }
+
+};
+
+console.log(arreyD);
